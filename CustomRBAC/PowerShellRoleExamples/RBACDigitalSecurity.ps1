71a85496ea4547886636cf67f03ab81ec279b077
Shannon Kuehn <shannon.kuehn@gmail.com>
2019-04-11 19:23:48 -0500

    Update RBACDigitalSecurity.ps1

--- CustomRBAC/PowerShellRoleExamples/RBACDigitalSecurity.ps1
+++ CustomRBAC/PowerShellRoleExamples/RBACDigitalSecurity.ps1
@@ -6,7 +6,7 @@ $ADGroupSearch = Get-AzADGroup -SearchString $ADGroup
 
 $scope = ""
 
-$role = Get-AzureRmRoleDefinition "Reader" 
+$role = Get-AzRoleDefinition "Reader" 
 $role.id = $null
 $role.name = "Digital Security"
 $role.Description = "Digital Security Role"
