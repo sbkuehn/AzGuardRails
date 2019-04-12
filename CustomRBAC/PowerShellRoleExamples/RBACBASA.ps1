a8762fa6bc060c0bc7ae5eb792f0c2fec56eeb06
Shannon Kuehn <shannon.kuehn@gmail.com>
2019-04-11 19:22:15 -0500

    Update RBACBASA.ps1

--- CustomRBAC/PowerShellRoleExamples/RBACBASA.ps1
+++ CustomRBAC/PowerShellRoleExamples/RBACBASA.ps1
@@ -6,7 +6,7 @@ $ADGroupSearch = Get-AzADGroup -SearchString $ADGroup
 
 $scope = ""
 
-$role = Get-AzureRmRoleDefinition "Reader" 
+$role = Get-AzRoleDefinition "Reader" 
 $role.id = $null
 $role.name = "BA SA"
 $role.Description = "Business Analyst Role"
