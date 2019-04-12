1dba6bb64833a411738de88fe3e628fcc6085af9
Shannon Kuehn <shannon.kuehn@gmail.com>
2019-04-11 19:36:21 -0500

    Update RoleTemplate.ps1

--- CustomRBAC/PowerShell/RoleTemplate.ps1
+++ CustomRBAC/PowerShell/RoleTemplate.ps1
@@ -1,1 +1,2 @@
-﻿Get-AzRoleDefinition -Name "Network Contributor" | ConvertTo-Json | Out-File "C:\ScriptOutputs\NetworkContributor.json"
+# Provides a baseline template to work off of with an existing builtin role.
+Get-AzRoleDefinition -Name "Network Contributor" | ConvertTo-Json | Out-File "C:\ScriptOutputs\NetworkContributor.json"
