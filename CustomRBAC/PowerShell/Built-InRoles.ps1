6dbba81a54bd993812c3fdd14ffee8e26c811da8
Shannon Kuehn <shannon.kuehn@gmail.com>
2019-04-11 19:29:17 -0500

    Update Built-InRoles.ps1

--- CustomRBAC/PowerShell/Built-InRoles.ps1
+++ CustomRBAC/PowerShell/Built-InRoles.ps1
@@ -1,1 +1,2 @@
-﻿Get-AzRoleDefinition | Select-Object Name, Description | Export-Csv c:\Scripts\allArmRoles.csv -NTI
+# Grabs all Azure Resource Manager role definitions, selects the name + description, then exports the results as a csv.
+Get-AzRoleDefinition | Select-Object Name, Description | Export-Csv c:\Scripts\allArmRoles.csv -NTI
