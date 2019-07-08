6257dfcff87948a89ec8f87d984fcf6ad3e6e53c
Shannon Kuehn <shannon.kuehn@gmail.com>
2019-07-08 10:42:51 -0500

    Update CustomRoleAssignment.ps1

--- CustomRBAC/PowerShell/CustomRoleAssignment.ps1
+++ CustomRBAC/PowerShell/CustomRoleAssignment.ps1
@@ -27,3 +27,5 @@ $role.AssignableScopes.Clear()
 $role.AssignableScopes.Add("/subscriptions/subscriptions/xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxx/resourceGroups/MyResourceGroup")
 
 New-AzRoleDefinition -Role $role
+
+adding
