037d466a07a54681db0533382cf8a9b1387171b1
Shannon Kuehn <shannon.kuehn@gmail.com>
2019-07-08 09:50:11 -0500

    Update RBACCloudOpsLead.ps1

--- CustomRBAC/PowerShellRoleExamples/RBACCloudOpsLead.ps1
+++ CustomRBAC/PowerShellRoleExamples/RBACCloudOpsLead.ps1
@@ -1,7 +1,7 @@
 ﻿$ADGroup = "Engineering"
 $ADGroupSearch = Get-AzureRmADGroup -SearchString $ADGroup
 
-#Scope should be subscription ID in form of "subscriptions/xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxx" : 
+#Scope should be subscription ID in the form of "subscriptions/xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxx" : 
 #Can only add one subscription in this form
 
 $scope = ""
