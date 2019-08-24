e3ce71482a3e20beacc57dfc6407e7bbac97a361
Shannon Kuehn <shannon.kuehn@gmail.com>
2019-08-24 15:35:13 -0500

    Update AzResourceProviderOperations.ps1

--- CustomRBAC/PowerShell/AzResourceProviderOperations.ps1
+++ CustomRBAC/PowerShell/AzResourceProviderOperations.ps1
@@ -1,39 +1,39 @@
 ﻿# Exports a list of all Azure Provider Operations to a CSV file.
-Get-AzResourceProviderOperation -OperationSearchString * | Select Operation,OperationName,ProviderNamespace,Description `
+Get-AzResourceProviderAction -OperationSearchString * | Select Operation,OperationName,ProviderNamespace,Description `
 | Export-Csv c:\Scripts\CustomRBAC\resourceprovideractions.csv -nti
 
 # Working examples to drill deeper into specific provider namespaces.
 Get-AzResourceProvider | Select ProviderNameSpace | Export-Csv C:\Scripts\CustomRBAC\ResourceProviders.csv -nti
 
-Get-AzResourceProviderOperation -OperationSearchString Microsoft.Automation/* `
+Get-AzResourceProviderAction -OperationSearchString Microsoft.Automation/* `
 | Select Operation,OperationName,Description | Export-Csv C:\Scripts\CustomRBAC\Automation.csv -nti
 
-Get-AzResourceProviderOperation -OperationSearchString Microsoft.Compute/* `
+Get-AzResourceProviderAction -OperationSearchString Microsoft.Compute/* `
 | Select Operation,OperationName,Description | Export-Csv C:\Scripts\CustomRBAC\Compute.csv -nti
 
-Get-AzResourceProviderOperation -OperationSearchString Microsoft.HDInsight/* `
+Get-AzResourceProviderAction -OperationSearchString Microsoft.HDInsight/* `
 | Select Operation,OperationName,Description | Export-Csv CC:\Scripts\CustomRBAC\HDInsight.csv -nti
 
-Get-AzResourceProviderOperation -OperationSearchString Microsoft.insights/* `
+Get-AzResourceProviderAction -OperationSearchString Microsoft.insights/* `
 | Select Operation,OperationName,Description | Export-Csv C:\Scripts\CustomRBAC\insights.csv -nti
 
-Get-AzResourceProviderOperation -OperationSearchString Microsoft.Network/* `
+Get-AzResourceProviderAction -OperationSearchString Microsoft.Network/* `
 | Select Operation,OperationName,Description | Export-Csv C:\Scripts\CustomRBAC\Network.csv -nti
 
-Get-AzResourceProviderOperation -OperationSearchString Microsoft.OperationalInsights/* `
+Get-AzResourceProviderAction -OperationSearchString Microsoft.OperationalInsights/* `
 | Select Operation,OperationName,Description | Export-Csv C:\Scripts\CustomRBAC\OpsInsights.csv -nti
 
-Get-AzResourceProviderOperation -OperationSearchString Microsoft.OperationsManagement/* `
+Get-AzResourceProviderAction -OperationSearchString Microsoft.OperationsManagement/* `
 | Select Operation,OperationName,Description | Export-Csv CC:\Scripts\CustomRBAC\OpsMgmt.csv -nti
 
-Get-AzResourceProviderOperation -OperationSearchString Microsoft.Sql/* `
+Get-AzResourceProviderAction -OperationSearchString Microsoft.Sql/* `
 | Select Operation,OperationName,Description | Export-Csv C:\Scripts\CustomRBAC\SQL.csv -nti
 
-Get-AzResourceProviderOperation -OperationSearchString Microsoft.Storage/* `
+Get-AzResourceProviderAction -OperationSearchString Microsoft.Storage/* `
 | Select Operation,OperationName.Description | Export-Csv C:\Scripts\CustomRBAC\Storage.csv -nti
 
-Get-AzResourceProviderOperation -OperationSearchString Microsoft.Resources/* `
+Get-AzResourceProviderAction -OperationSearchString Microsoft.Resources/* `
 | Select Operation,OperationName,Description | Export-Csv C:\Scripts\CustomRBAC\Resources.csv -nti
 
-Get-AzResourceProviderOperation -OperationSearchString '*' | ? { $_.Operation -like 'Microsoft.Network/*' } `
+Get-AzResourceProviderAction -OperationSearchString '*' | ? { $_.Operation -like 'Microsoft.Network/*' } `
 | select Operation,OperationName,Description | Out-File azure-network-permissions.txt
